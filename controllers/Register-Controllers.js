9c02b7226de9f4b4c4df7202530e56eea57b118e
abbymakarim <abby.makarim@gmail.com>
2020-12-16 17:49:01 +0700

    Added password hashing

--- controllers/Register-Controllers.js
+++ controllers/Register-Controllers.js
@@ -4,6 +4,23 @@ class Controller {
     static registpage(req, res){
         res.render('registerForm.ejs')
     } 
+
+    static postRegister(req, res){
+        const user = {
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+            phone_number: req.body.phone_number,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }
+        User.create(user)
+        .then(data => {
+            console.log(data)
+            res.redirect('/login')
+        })
+        .catch(err => console.log(err))
+    }
 }
 
 
