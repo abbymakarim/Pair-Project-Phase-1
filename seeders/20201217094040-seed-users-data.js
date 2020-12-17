c4ab79c24ee597d539b14e0806b08c42087adbed
mrdanang <mrdanang4108@gmail.com>
2020-12-17 16:57:15 +0700

    pair project

---
+++ seeders/20201217094040-seed-users-data.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const data = [
+  {
+    username : "project1",
+    password : "pairproject1",
+    email : "pairproject1@email.com",
+    phone_number : "08123456789"
+  }
+]
+
+data.forEach(el => {
+  el.createdAt = new Date()
+  el.updatedAt = new Date()
+})
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+    return queryInterface.bulkInsert('Users', data)
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+
+    return queryInterface.bulkDelete('Users', null, {})
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
